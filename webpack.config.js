4e557f217c53614e48652e57724646ee0b65a4b0
Johan Lindskogen <johan.lindskogen@gmail.com>
2016-02-05 09:35:58 +0100

    Public path should be relative

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     output: {
         path: __dirname + "/build",
         filename: "bundle.js",
-        publicPath: __dirname + "/build/"
+        publicPath: "build/"
     },
     module: {
         loaders: [
