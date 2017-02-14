4da77db9b5d78dc1ce392e7f2aa69e1af6d164fe
Johan Lindskogen <johan.lindskogen@gmail.com>
2017-02-14 23:57:40 +0100

    use extract text plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,4 @@
-var precss = require("precss");
-var autoprefixer = require("autoprefixer");
+const precss = require("precss");
+const autoprefixer = require("autoprefixer");
 
 module.exports = { plugins: [ precss, autoprefixer ] };
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
   entry: "./entry.js",
   output: {
@@ -7,8 +9,16 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.css$/, use: [ "style-loader", "css-loader", "postcss-loader" ] },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader!postcss-loader",
+          publicPath: ""
+        })
+      },
       { test: /\.(jpg|png)$/, loader: "file-loader" }
     ]
-  }
+  },
+  plugins: [ new ExtractTextPlugin("style.css") ]
 };
