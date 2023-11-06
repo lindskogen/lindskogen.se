ac516a5023d518c385e2deb6374a886de2612b97
Johan Lindskogen <johan.lindskogen@gmail.com>
2023-11-06 21:11:52 +0100

    Bump astro

--- src/pages/me.json.js
+++ src/pages/me.json.js
@@ -1,11 +1,10 @@
-export async function get () {
-  return {
-    body: JSON.stringify({
+export async function GET() {
+  return new Response(
+    JSON.stringify({
       "GitHub": "https://github.com/lindskogen",
       "Twitter": "https://twitter.com/jlindsk",
       "Mastodon": "https://mastodon.nu/@jlindsk",
       "LinkedIn": "https://se.linkedin.com/pub/johan-lindskogen/70/652/176/",
       "CV": "https://lindskogen.se/cv"
-    })
-  }
+    }))
 }
