8804a6c96f34577ddc23715d5c4aa3fde70562f0
Johan Lindskogen <johan.lindskogen@gmail.com>
2024-11-25 20:10:39 +0100

    Replace twitter with bluesky

--- src/pages/me.json.js
+++ src/pages/me.json.js
@@ -2,7 +2,7 @@ export async function GET() {
   return new Response(
     JSON.stringify({
       "GitHub": "https://github.com/lindskogen",
-      "Twitter": "https://twitter.com/jlindsk",
+      "Bluesky": "https://bsky.app/profile/jlindsk.bsky.social",
       "Mastodon": "https://mastodon.nu/@jlindsk",
       "LinkedIn": "https://se.linkedin.com/pub/johan-lindskogen/70/652/176/",
       "CV": "https://lindskogen.se/cv"
